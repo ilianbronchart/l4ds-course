c2190647e56c78755b86d78692f815b82534e6da
Ilian Bronchart <ilianbronchart@protonmail.com>
2023-07-16 17:36:53 +0200

    Fix github pages

--- data-workflow/generate_report.py
+++ data-workflow/generate_report.py
@@ -16,14 +16,14 @@ latest_report = sorted(report_dirs)[-1]
 timestamp = os.path.basename(latest_report).split('-')[1].replace(':', '-')
 html = template.render(timestamp=timestamp)
 
-if not os.path.exists('./docs'):
-    os.makedirs('./docs')
+if not os.path.exists('../docs'):
+    os.makedirs('../docs')
 
 # Copy all files from latest_report directory to docs directory
 for file_name in os.listdir(f'./reports/{latest_report}'):
-    shutil.copy(f'./reports/{latest_report}/{file_name}', f'./docs/{file_name}')
+    shutil.copy(f'./reports/{latest_report}/{file_name}', f'../docs/{file_name}')
 
 # Save report.html to docs directory
-with open(f'./docs/index.html', 'w') as f:
+with open(f'../docs/index.html', 'w') as f:
     f.write(html)
 
